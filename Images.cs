797141e37b8767d4c1ef9bc0b19800c42126b0c2
evaderrama <evalderrama@domingoalonsogroup.com>
2023-06-06 22:43:32 +0100

    :necktie: Add SetupGrid

--- Images.cs
+++ Images.cs
@@ -7,12 +7,12 @@ namespace SnakeGame
 {
     public static class Images
     {
-        private static readonly ImageSource Empty = LoadImage("Empty.png");
-        private static readonly ImageSource Body = LoadImage("Body.png");
-        private static readonly ImageSource Head = LoadImage("Head.png");
-        private static readonly ImageSource Food = LoadImage("Food.png");
-        private static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
-        private static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
+        public static readonly ImageSource Empty = LoadImage("Empty.png");
+        public static readonly ImageSource Body = LoadImage("Body.png");
+        public static readonly ImageSource Head = LoadImage("Head.png");
+        public static readonly ImageSource Food = LoadImage("Food.png");
+        public static readonly ImageSource DeadBody = LoadImage("DeadBody.png");
+        public static readonly ImageSource DeadHead = LoadImage("DeadHead.png");
 
 
         private static ImageSource LoadImage(string filename)
